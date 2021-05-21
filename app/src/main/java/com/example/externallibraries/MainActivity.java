bd659e71b02aca121b9ebd9b101467e2122e9e06
c20emili <c20emili@student.his.se>
2021-05-21 10:22:04 +0200

    added a simple progressbar in xml and removed extra import in main

--- app/src/main/java/com/example/externallibraries/MainActivity.java
+++ app/src/main/java/com/example/externallibraries/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.cliff.comparingperformancebar.PercentageProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
